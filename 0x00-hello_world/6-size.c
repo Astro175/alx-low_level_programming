334d8fafa3e2fe05a2a00dddd281f4f0b0c315b7
Astro175 <aspark400@gmail.com>
2022-10-14 21:32:55 -0700

    new

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of char: %lu byte(s)\n", sizeof(int));
+printf("Size of char: %lu byte(s)\n", sizeof(long int));
+printf("Size of char: %lu byte(s)\n", sizeof(float));
+return (0);
+}
