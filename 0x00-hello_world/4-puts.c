45cf6bda68e6e5708e60e9805f2aeb9132ccda67
Astro175 <aspark400@gmail.com>
2022-10-14 19:55:27 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,6 @@
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+#include <stdio.h>
 
-int main(void)
-
-{
-            puts("Programming is like building a multilingual puzzle");
-	    return (0);
-
+int main() {
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
