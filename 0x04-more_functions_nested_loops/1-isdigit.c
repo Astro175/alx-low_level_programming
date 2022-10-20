b375bc58336b1953bad0134599db46a0adc82756
Astro175 <aspark400@gmail.com>
2022-10-20 16:01:50 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry
+ * @c: v
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c >= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
