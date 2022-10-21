7fb33b666bd0945b320c927e13bdea8daea93642
Astro175 <aspark400@gmail.com>
2022-10-21 15:31:16 -0700

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (n = 0; i <= 14; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
