72df366e33cb24027573c3f5021329190cb5bdcd
Astro175 <aspark400@gmail.com>
2022-10-27 14:26:51 -0700

    new

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper- Entry
+ * Return: 0
+ * @a: char
+ */
+
+char *string_toupper(char *a)
+{
+	int n = 0;
+
+	while (a[n] != '\0')
+	{
+		if (a[n] >= 'a' && a[n] <= 'z')
+		{
+			a[n] -= 32;
+			n++;
+		}
+	}
+	return (a);
+}
