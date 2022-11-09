ea61c54f07f3cb1c7e600817b961f2fe41de12fc
Astro175 <aspark400@gmail.com>
2022-11-08 20:54:40 -0800

    new

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * alloc_grid - Entry
+  * @width: c1
+  * @height: c2
+  * Return: 0
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int *me;
+
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	me = 
+
+  
