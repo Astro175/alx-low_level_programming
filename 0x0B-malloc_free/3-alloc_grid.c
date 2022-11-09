5b20e1924330e3b68f5c8ce73a7abdf9e51bf154
Astro175 <aspark400@gmail.com>
2022-11-08 21:19:32 -0800

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,13 +9,39 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *me;
+	int **me;
 
 	int x, y;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	me = 
+	me = malloc(sizeof(int *) * height);
 
-  
+	if (me == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		me[x] = malloc(sizeof(int) * width);
+
+		if (me[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(me[x]);
+
+			free(me);
+
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			me[x][y] = 0;
+	}
+	return (me);
+}
