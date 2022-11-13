66aa92ff343298db93ca0d58678ec8de844626df
Astro175 <aspark400@gmail.com>
2022-11-13 11:20:56 -0800

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[length2] != '\0')
 		length2++;
 
-	if (n > length2)
+	if (n >= length2)
 		n = length2;
 	ptr = malloc((length1 + n + 1) * sizeof(char));
 
