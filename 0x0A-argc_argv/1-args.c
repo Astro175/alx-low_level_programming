cced4ed71a628179139d9cd387e379ddf4268324
Astro175 <aspark400@gmail.com>
2022-11-20 22:56:34 -0800

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry
+  * @argc: argc
+  * @argv: argv
+  * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc);
+	return (0);
+}
