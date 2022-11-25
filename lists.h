9ac4440dea3267ddeaf6fb6fa6e3ccafdef8aeaf
Astro175 <aspark400@gmail.com>
2022-11-24 20:05:03 -0800

    singly linked list

--- lists.h
+++ lists.h
@@ -1,6 +1,6 @@
 #ifndef _LIST_H
 #define _LIST_H
-
+#include <stdlib.h>
 /**
   * struct list_s - singly linked list
   * @str : string
@@ -14,7 +14,7 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_s
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
