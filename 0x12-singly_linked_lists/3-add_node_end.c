5e73aed2e9c0b63d9590ba6de98127d6cfdc88bb
Astro175 <aspark400@gmail.com>
2022-11-25 03:01:32 -0800

    new

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists_h"
+
+/**
+  * add_node_end - Adds new node at the end of thr list
+  * @head: Head of the node
+  * @str: string to be passed into the new node
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+	size_t length;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+
+	for(length = 0; str[length]; length++)
+		;
+
+	new->len = length;
+	new->next = NULL;
+
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		head = new*;
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	return (*head);
+}
