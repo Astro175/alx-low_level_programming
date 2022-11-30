38a463fa3fc7b47d980101ed03b7b4448084771b
Astro175 <aspark400@gmail.com>
2022-11-29 20:13:00 -0800

    task 6 done!

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+  * pop_listint- Deletes the first node
+  * @head: Pointer pointing to the first node
+  * Return: Data of the first node
+  */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+        listint_t *ptr2;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	ptr2 = *head;
+
+	n = ptr2->n;
+
+	temp = ptr2->next;
+
+	free(ptr2);
+
+	*head = temp;
+
+	return (n);
+}
