32dd9d1145648fb41dabc2a22eb3d60e73d949e3
Astro175 <aspark400@gmail.com>
2022-12-03 23:34:08 -0800

    Added new task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * get_endianness - Gets the endianness of a computer
+  * Return: the endian number
+  */
+
+int get_endianness(void)
+{
+	unsigned int e = 1;
+	char *p;
+
+	p = (char *) &e;
+
+	return ((int *)*e);
+}
