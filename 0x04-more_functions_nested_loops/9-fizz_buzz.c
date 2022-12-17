4f76ad6d191b8b203d0e119e456159fff86615d0
Astro175 <aspark400@gmail.com>
2022-12-17 06:28:00 +0100

    Updated some tasks in the more function directory

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,16 @@ int main(void)
         
 	for (n = 1; n <= 100; n++)
 	{
-		printf(" ");
-		if (n % 3 == 0)
-			printf("Fizz");
-		if (n % 5 == 0)
-			printf("Buzz");
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
-		if (n % 3 != 0 && n % 5 != 0)
-			printf("%d", n);
+        {
+            printf("FizzBuzz ");
+        }
+        else if (n % 3 == 0)
+            printf("Fizz ");
+        else if (n % 5 == 0)
+            printf("Buzz ");
+        else
+            printf("%d ", n);
 	}
 	printf("\n");
 	return (0);
