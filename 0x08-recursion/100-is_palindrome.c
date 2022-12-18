e1d7028e5f82bc992b9e70ddd55737e1c04adc4a
Astro175 <aspark400@gmail.com>
2022-12-18 13:46:36 +0100

    some advanced tasks done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,16 +10,16 @@
 
 int is_palindrome(char *s)
 {
-        int len;
+	int len;
 
-        len = strlen(s);
+	len = strlen(s);
 
-        for (int i = 0; i < len / 2; i++)
-        {
-                if (s[i] != s[len - i -1])
-                {
-                        return 0;
-                }
-        }
-        return 1;
+	for (int i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - i - 1])
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
