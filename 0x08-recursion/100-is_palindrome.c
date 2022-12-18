d5e52650719612ad2d8f50b016e2e070d1fd7d0a
Astro175 <aspark400@gmail.com>
2022-12-18 13:21:38 +0100

    Finished some new advanced tasks

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * is_palindrome- Checks if a string is palindrome or not
+ * @s: string
+ * Return: returns 1 if it is palindrome and 0, if it is not
+ */
+
+int is_palindrome(char *s)
+{
+        int len;
+
+        len = strlen(s);
+
+        for (int i = 0; i < len / 2; i++)
+        {
+                if (s[i] != s[len - i -1])
+                {
+                        return 0;
+                }
+        }
+        return 1;
+}
