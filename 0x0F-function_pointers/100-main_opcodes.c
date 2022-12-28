cdaaf736e627bafc1cd8828c5fb346802ac68061
Astro175 <aspark400@gmail.com>
2022-12-28 07:45:01 +0100

    new

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < num_bytes; i++)
 	{
-		printf("%02x", *((unsigned char*)main_addr + i));
+		printf("%02x", *((unsigned char *)main_addr + i));
 		if (i != num_bytes - 1)
 			printf(" ");
 	}
