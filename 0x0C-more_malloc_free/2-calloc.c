17a44df1bafb795534838317c2883be498fb593f
Astro175 <aspark400@gmail.com>
2022-12-28 11:10:46 +0100

    new

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_calloc - Function that preserves space for an array
+ * @nmemb: number of members
+ * @size: size of the array
+ * Return: a void pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		((char *)ptr)[i] = 0;
+
+	return (ptr);
+}
