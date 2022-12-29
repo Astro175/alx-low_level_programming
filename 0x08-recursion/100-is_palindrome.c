612e5ef17ca8f73dcc04356032b142e97ad0d124
Astro175 <aspark400@gmail.com>
2022-12-29 10:17:20 +0100

    new

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,11 +12,12 @@ int is_palindrome(char *s)
 {
 	char *substr;
 	int result;
+
 	if (*s == '\0' || *(s + 1) == '\0')
-		return 1;
+		return (1);
 
 	if (*s != *(s + strlen(s) - 1))
-		return 0;
+		return (0);
 
 	substr = malloc(strlen(s) - 2 + 1);
 	memcpy(substr, s + 1, strlen(s) - 2);
@@ -24,5 +25,5 @@ int is_palindrome(char *s)
 
 	result = is_palindrome(substr);
 	free(substr);
-	return result;
+	return (result);
 }
