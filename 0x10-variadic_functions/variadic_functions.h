9aab8f0627bc9c5e9455a43177c2d721e83ef858
Astro175 <aspark400@gmail.com>
2022-12-31 14:17:34 +0100

    new task done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,22 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_num(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
+
+/**
+ * struct printer - a new struct
+ * @symbol: symbol
+ * @f: function to print
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list args);
+} printer_t;
 #endif
