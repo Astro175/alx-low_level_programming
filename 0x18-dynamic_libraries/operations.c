b6ff181c0cbfc3af4ffe3091fa592b697d90e627
Astro175 <aspark400@gmail.com>
2023-01-16 17:09:29 +0100

    advanced task in dynamic libraries

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: num1
+ * @b: num2
+ * Return: resu;t
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two numbers
+ * @a: num1
+ * @b: num2
+ * Return: resu;t
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
