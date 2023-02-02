338e1d88611dd8b205f33ace2c17c89b93d6770b
Astro175 <aspark400@gmail.com>
2023-02-02 23:05:59 +0100

    fixed some bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while(temp)
+	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
